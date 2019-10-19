4cda166e6cea9806b675f261c0b97574e612ba47
dangduyhien1234 <ahien1512@gmail.com>
2019-10-19 20:57:09 +0700

    lam bai 1

---
+++ Buoi_1/Bai_1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+	printf("Dang Duy Hien"); //In ra màn hình
+	printf("\n 2001"); // ky tu \n xuong dong
+	printf("\n Que \t quan: Nam \t Dinh"); // ky tu \t: tab
+	printf("\\n"); // \\-> '\'
+	getch();//getch: dung man hinh
+	
+}
