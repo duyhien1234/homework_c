d8a219c30e3fa1cc9d9b387d3954af287e6688a5
dangduyhien1234 <ahien1512@gmail.com>
2019-10-19 09:32:58 +0700

    add demo

---
+++ HelloWord.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+	//In ra man hinh dong chu Hello World
+	printf("Hello World! \n"); //printf: Lenh in ra man hinh
+	printf("Dang Duy Hien \n");
+	printf("Dang\t Duy\t hien");
+	printf("\\n");
+	getch(); //Dung ma hinh
+//	return 0;
+/*
+\n; Xuong dong
+\t: tab
+\\: \
+*/
+}
