0e87843d193e6105abc62454d456c633e387001d
dangduyhien1234 <ahien1512@gmail.com>
2019-11-01 07:58:55 +0700

    add file

--- Buoi_1/Bai_1.c
+++ Buoi_1/Bai_1.c
@@ -1,11 +1,26 @@
 #include<stdio.h>
 #include<conio.h>
+#include<string.h>
+#include<stdlib.h>
 
 void main(){
-	printf("Dang Duy Hien"); //In ra màn hình
-	printf("\n 2001"); // ky tu \n xuong dong
-	printf("\n Que \t quan: Nam \t Dinh"); // ky tu \t: tab
-	printf("\\n"); // \\-> '\'
-	getch();//getch: dung man hinh
+	//Nhap tu ban phim ten, tuoi, nam sinh -> in ra man hinh
+	//clrscr(); //lam sach man hinh
+	system("cls"); //Lam sach man hinh
+	// Khai bao cac bien
+	char name[30]; //vi khong biet ten nhap vao dài bao nhieu nen khai bao do dai 30 là khoang hop ly
+	int age, year;
+	printf("Nhap ten cua ban = ");
+	gets(name); //gan gia tri vao cho bien name
+	printf("Nhap tuoi cua ban = ");
+	scanf("%d", &age); //"%d": lay gia tri kieu int
+	printf("Nhap nam sinh cua ban = ");
+	scanf("%d", &year);
+	
+	//In ra man hinh
+	printf("Thong tin cua ban la:\n");
+	printf("Ten: %s, tuoi: %d, nam sinh: %d", name, age, year);
+	getch();
+	
 	
 }
