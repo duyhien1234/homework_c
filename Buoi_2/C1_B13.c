e1ed83503695aea8da66899dce00cff1a42db397
dangduyhien1234 <ahien1512@gmail.com>
2019-11-04 22:33:37 +0700

    bài 13 chương 1

---
+++ Buoi_2/C1_B13.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+void main(){
+	/*
+	bai 13 chuong 1
+	cac phep toan deu co dang x mu (2n), thuc hien n lan
+	=> nhap x, n 
+	=> dung vong lap for de tinh ket qua 
+	*/
+	int i, x, n;
+	long sum = 0;
+	printf("nhap so lan lap n = ");
+	scanf("%d", &n);
+		printf("nhap x = ");
+	scanf("%d", &x);
+	for (i = 1; i<= n; i++){
+		sum +=(long)pow(x, (2 * i));
+	}
+	printf("ket qua s = %ld", sum);
+}
+		
+	
+	 
+	
+	
+
+	
+
+
