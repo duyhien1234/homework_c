f3e17a51a30e0bdf65df2ea0c0657ef9fcf6003f
dangduyhien1234 <ahien1512@gmail.com>
2019-11-06 20:33:56 +0700

    bài 789

---
+++ Buoi_2/bai7_chuong1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+void main(){
+	/*
+	nhap n va tinh s
+	*/
+	int i, n;
+	float S = 0;
+	printf("nhap n");
+	scanf("%d", &n);
+	i=1;
+	while(i<=n){
+		S=S + (float) i/(i+1);
+		i++;
+	}
+	printf("in ra man hinh S = %f", S);
+	
+	
+	
+}
